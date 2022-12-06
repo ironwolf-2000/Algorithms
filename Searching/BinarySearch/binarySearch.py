b5281f36aa87fb09e2f62370881d6214c8b7f192
Ironwolf <michaelvologodskiy@gmail.com>
2022-12-06 08:55:30 +0300

    Update binarySearch

--- Searching/BinarySearch/binarySearch.py
+++ Searching/BinarySearch/binarySearch.py
@@ -34,6 +34,28 @@ def bisectRight(nums: list[int], target: int) -> int:
     return lo
 
 
+# similar to indexOf in other languages
+def index(nums: list[int], target: int) -> int:
+    """
+    N = len(nums)
+    -------------
+    Time: O(logN)
+    Space: O(1)
+    """
+    step = len(nums) // 2
+    i = -1
+
+    while step:
+        while i + step < len(nums) and nums[i + step] < target:
+            i += step
+        step //= 2
+
+    if i + 1 == len(nums) or nums[i + 1] != target:
+        return -1
+
+    return i + 1
+
+
 nums = [1, 2, 3, 4, 7, 9]
 
 print(bisectLeft(nums, 4))  # 3
@@ -44,3 +66,13 @@ print(bisectRight(nums, 0))  # 0
 
 print(bisectLeft(nums, 10))  # 6
 print(bisectRight(nums, 10))  # 6
+
+print("-------------------")
+
+nums = [1, 2, 3, 3, 3, 4, 7, 9]
+
+print(index(nums, 0))  # -1
+print(index(nums, 1))  # 0
+print(index(nums, 3))  # 2
+print(index(nums, 9))  # 7
+print(index(nums, 10))  # -1
