93371c2162d33c2949f7eea3502319a7d00a76e0
Ironwolf <michaelvologodskiy@gmail.com>
2022-12-17 08:50:47 +0300

    Add dfs version of topologicalSort

---
+++ Graphs/TopologicalSort/dfs.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+
+def topsort(graph: dict[list[int]]) -> list[int]:
+    """
+    V = number of vertices in the graph
+    E = number of edges in the graph
+    -------------
+    Time: O(V + E)
+    Space: O(V)
+    """
+
+    def dfs(u: int) -> None:
+        color[u] = 1  # visited
+
+        for v in graph[u]:
+            if color[v] == 1 or not color[v] and dfs(v):
+                return True  # cycle found
+
+        color[u] = 2  # fully explored
+        res.append(u)
+
+        return False  # no cycle found
+
+    color = defaultdict(int)
+    res = []
+
+    for u in graph:
+        if not color[u] and dfs(u):
+            return "Cycle found!"
+
+    return res[::-1]
+
+
+graph = {
+    1: [2, 4],
+    2: [4],
+    3: [],
+    4: [3, 8],
+    5: [],
+    6: [5],
+    7: [],
+    8: [],
+}
+print(topsort(graph))  # [7, 6, 5, 1, 2, 4, 8, 3]
+
+graph = {
+    1: [2],
+    2: [3],
+    3: [1],
+}
+print(topsort(graph))  # Cycle found!
