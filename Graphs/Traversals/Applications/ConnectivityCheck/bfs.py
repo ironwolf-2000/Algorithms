f2050efdb96b2fc2633abf33b5ea55ad006140b3
Ironwolf <michaelvologodskiy@gmail.com>
2022-12-27 21:45:37 +0300

    Add connectivityCheck

---
+++ Graphs/Traversals/Applications/ConnectivityCheck/bfs.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+def check(graph: dict[int, list[int]]) -> bool:
+    """
+    V = number of vertices in the graph
+    E = number of edges in the graph
+    -------------
+    Time: O(V + E)
+    Space: O(V)
+    """
+
+    start = list(graph.keys())[0]
+
+    q = deque([start])
+    visited = {start}
+
+    while q:
+        u = q.popleft()
+        for v in graph[u]:
+            if v not in visited:
+                q.append(v)
+                visited.add(v)
+
+    return len(visited) == len(graph)
+
+
+graph = {
+    1: [3, 4],
+    2: [5],
+    3: [1, 4],
+    4: [1, 3],
+    5: [2],
+}
+print(check(graph))  # False
+
+graph = {
+    1: [3, 4],
+    2: [3, 5],
+    3: [1, 2, 4],
+    4: [1, 3],
+    5: [2],
+}
+print(check(graph))  # True
---
+++ Graphs/Traversals/Applications/ConnectivityCheck/dfs.py
@@ -0,0 +1,40 @@
+def check(graph: dict[int, list[int]]) -> bool:
+    """
+    V = number of vertices in the graph
+    E = number of edges in the graph
+    -------------
+    Time: O(V + E)
+    Space: O(V)
+    """
+
+    def dfs(u: int) -> None:
+        visited.add(u)
+        for v in graph[u]:
+            if v not in visited:
+                dfs(v)
+
+    visited = set()
+    start = list(graph.keys())[0]
+
+    dfs(start)
+
+    return len(visited) == len(graph)
+
+
+graph = {
+    1: [3, 4],
+    2: [5],
+    3: [1, 4],
+    4: [1, 3],
+    5: [2],
+}
+print(check(graph))  # False
+
+graph = {
+    1: [3, 4],
+    2: [3, 5],
+    3: [1, 2, 4],
+    4: [1, 3],
+    5: [2],
+}
+print(check(graph))  # True
