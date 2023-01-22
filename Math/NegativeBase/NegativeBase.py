86783dc2b014f508a0cbae2d37c1412e55050b69
Ironwolf <michaelvologodskiy@gmail.com>
2023-01-22 23:00:05 +0300

    Add NegativeBase addition

--- Math/NegativeBase/NegativeBase.py
+++ Math/NegativeBase/NegativeBase.py
@@ -1,17 +1,22 @@
 class NegativeBase:
-    def __init__(self, base: int, val: int) -> None:
+    def __init__(self, base: int, num: list[int]) -> None:
         """
         val: number in decimal base
         base: target negative base
         """
         self._base = base
-        self._num = self._encode(val)
+        self._num = num
+        self._decimal_num = self._to_decimal(num)
 
     @property
     def num(self):
         return self._num
 
-    def _encode(self, val: int) -> list[int]:
+    @property
+    def decimal_num(self):
+        return self._decimal_num
+
+    def to_initial(self, val: int) -> list[int]:
         res = []
 
         while val:
@@ -24,17 +29,45 @@ class NegativeBase:
 
         return res[::-1] or [0]
 
-    def decode(self) -> int:
+    def _to_decimal(self, num: list[int]) -> int:
         res = 0
 
-        for x in self._num:
+        for x in num:
             res = res * self._base + x
 
         return res
 
+    def __add__(self, other: "NegativeBase") -> "NegativeBase":
+        A, B = self.num[:], other.num[:]
+        carry = 0
+        res = []
 
-nb = NegativeBase(-10, 8163)
-nb = NegativeBase(-10, -8163)
-nb = NegativeBase(-10, 0)
+        while A or B or carry:
+            d1, d2 = (A or [0]).pop(), (B or [0]).pop()
+            sm = d1 + d2 + carry
+            d = sm % -self._base
 
-print(nb.decode())
+            res.append(d)
+            carry = (sm - d) // self._base
+
+        while len(res) > 1 and res[-1] == 0:
+            res.pop()
+
+        return NegativeBase(self._base, res[::-1])
+
+
+# Representation
+nb1 = NegativeBase(-10, [1, 2, 2, 4, 3])
+print(nb1.decimal_num)  # 8163
+
+nb2 = NegativeBase(-10, [9, 9, 7, 7])
+print(nb2.decimal_num)  # -8163
+
+nb3 = NegativeBase(-10, [1, 2, 3])  # 83
+print(nb3.decimal_num)
+
+
+# Addition
+print((nb1 + nb2).decimal_num)  # 0
+print((nb1 + nb3).decimal_num)  # 8246
+print((nb2 + nb3).decimal_num)  # -8080
